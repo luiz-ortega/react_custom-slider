584dfbab5731a27c246c27cabfa9740a12c6177b
luiz-ortega <luizngust@hotmail.com>
2021-01-17 16:28:01 -0300

    feat: add event trigger boundaries to component div

--- src/components/CustomSlider/index.tsx
+++ src/components/CustomSlider/index.tsx
@@ -1,33 +1,34 @@
-import React, { MouseEvent, useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, MouseEvent } from 'react';
 
 const CustomSlider: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
-  /* const logMousePosition = (event: MouseEvent) => {
-    setX(event.clientX);
-    setY(event.clientY);
-  }; */
-
   useEffect(() => {
-    window.addEventListener('mousemove', event => {
-      setX(event.clientX);
-      setY(event.clientY);
-    });
-
-    return () => {
-      window.removeEventListener('mousemove', event => {
+    console.log(containerRef);
+    if (containerRef && containerRef.current) {
+      containerRef.current.addEventListener('mousemove', event => {
         setX(event.clientX);
         setY(event.clientY);
       });
-    };
+
+      return () => {
+        containerRef &&
+          containerRef.current &&
+          containerRef.current.removeEventListener('mousemove', event => {
+            setX(event.clientX);
+            setY(event.clientY);
+          });
+      };
+    }
   }, []);
 
   return (
-    <>
+    <div style={{ border: '2px solid blue' }} ref={containerRef}>
       <h1>{`X - ${x}`}</h1>
       <h1>{`Y - ${y}`}</h1>
-    </>
+    </div>
   );
 };
 
