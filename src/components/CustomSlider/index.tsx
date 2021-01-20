65453b6bfa09a55c26ac20d2a57f33225c1b1927
luiz-ortega <luizngust@hotmail.com>
2021-01-19 21:49:56 -0300

    feat: copy styles from css only slider

--- src/components/CustomSlider/index.tsx
+++ src/components/CustomSlider/index.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useRef, MouseEvent } from 'react';
 
+import './style.css';
+
+const slides = [1, 2, 3, 4, 5];
+
 const CustomSlider: React.FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [x, setX] = useState(0);
@@ -25,10 +29,89 @@ const CustomSlider: React.FC = () => {
   }, []);
 
   return (
-    <div style={{ border: '2px solid blue' }} ref={containerRef}>
-      <h1>{`X - ${x}`}</h1>
-      <h1>{`Y - ${y}`}</h1>
-    </div>
+    <>
+      <p>{`X - ${x}/ Y - ${y}`}</p>
+      <section ref={containerRef} className="carousel" aria-label="Gallery">
+        <ol className="carousel__viewport">
+          <li id="carousel__slide1" className="carousel__slide">
+            <div className="carousel__snapper">
+              <a href="#carousel__slide4" className="carousel__prev">
+                Go to last slide
+              </a>
+              <a href="#carousel__slide2" className="carousel__next">
+                Go to next slide
+              </a>
+            </div>
+          </li>
+          <li id="carousel__slide2" className="carousel__slide">
+            <div className="carousel__snapper">
+              <a href="#carousel__slide1" className="carousel__prev">
+                Go to previous slide
+              </a>
+              <a href="#carousel__slide3" className="carousel__next">
+                Go to next slide
+              </a>
+            </div>
+          </li>
+          <li id="carousel__slide3" className="carousel__slide">
+            <div className="carousel__snapper">
+              <a href="#carousel__slide2" className="carousel__prev">
+                Go to previous slide
+              </a>
+              <a href="#carousel__slide4" className="carousel__next">
+                Go to next slide
+              </a>
+            </div>
+          </li>
+          <li id="carousel__slide4" className="carousel__slide">
+            <div className="carousel__snapper">
+              <a href="#carousel__slide3" className="carousel__prev">
+                Go to previous slide
+              </a>
+              <a href="#carousel__slide1" className="carousel__next">
+                Go to first slide
+              </a>
+            </div>
+          </li>
+        </ol>
+        <aside className="carousel__navigation">
+          <ol className="carousel__navigation-list">
+            <li className="carousel__navigation-item">
+              <a
+                href="#carousel__slide1"
+                className="carousel__navigation-button"
+              >
+                Go to slide 1
+              </a>
+            </li>
+            <li className="carousel__navigation-item">
+              <a
+                href="#carousel__slide2"
+                className="carousel__navigation-button"
+              >
+                Go to slide 2
+              </a>
+            </li>
+            <li className="carousel__navigation-item">
+              <a
+                href="#carousel__slide3"
+                className="carousel__navigation-button"
+              >
+                Go to slide 3
+              </a>
+            </li>
+            <li className="carousel__navigation-item">
+              <a
+                href="#carousel__slide4"
+                className="carousel__navigation-button"
+              >
+                Go to slide 4
+              </a>
+            </li>
+          </ol>
+        </aside>
+      </section>
+    </>
   );
 };
 
